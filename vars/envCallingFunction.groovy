cdb7fbf158f538e476d820fb1f25da0a7c506fe9
hi-to-cloud <hi.to.cloud@gmail.com>
2025-01-21 21:11:53 +0530

    Update envCallingFunction.groovy

--- vars/envCallingFunction.groovy
+++ vars/envCallingFunction.groovy
@@ -11,6 +11,7 @@ def call(){
                 steps {
                     envFromJenkinsfile()
                     echo "$NK"
+                    echo "$TKN"
                 }
             }
         }
