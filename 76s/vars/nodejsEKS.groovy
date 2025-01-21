4b1cabbfedd790f4eb50f5bc04afe4038ddc71d7
hi-to-cloud <hi.to.cloud@gmail.com>
2025-01-21 22:39:59 +0530

    Update nodejsEKS.groovy

--- 76s/vars/nodejsEKS.groovy
+++ 76s/vars/nodejsEKS.groovy
@@ -1,122 +1,150 @@
 def call(Map configMap){
-    // mapName.get("key-name")
-    def component = configMap.get("component")
-    echo "component is : $component"
     pipeline {
-        agent { node { label 'AGENT-1' } }
+        agent {
+            label 'AGENT-1'
+        }
+        options {
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds()
+            ansiColor('xterm')
+        }
         environment{
-            //here if you create any variable you will have global access, since it is environment no need of def
-            packageVersion = ''
+            def appVersion = '' //variable declaration
+            nexusUrl = pipelineGlobals.nexusURL()
+            region = pipelineGlobals.region()
+            account_id = pipelineGlobals.account_id()
+            component = configMap.get("component")
+            project = configMap.get("project")
+            def releaseExists = ""
         }
-        parameters {
-            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
-            
-            booleanParam(name: 'Destroy', defaultValue: false, description: 'Toggle this value')
-        }
-        
         stages {
-            stage('Get version'){
+            stage('read the version'){
                 steps{
                     script{
-                        def packageJson = readJSON(file: 'package.json')
-                        packageVersion = packageJson.version
-                        echo "version: ${packageVersion}"
+                        echo sh(returnStdout: true, script: 'env')
+                        def packageJson = readJSON file: 'package.json'
+                        appVersion = packageJson.version
+                        echo "application version: $appVersion"
+
                     }
                 }
             }
-            stage('Install depdencies') {
+            stage('Install Dependencies') {
                 steps {
-                    sh 'npm install'
+                sh """
+                    npm install
+                    ls -ltr
+                    echo "application version: $appVersion"
+                """
                 }
             }
-            stage('Unit test') {
-                steps {
-                    echo "unit testing is done here"
+            stage('Build'){
+                steps{
+                    sh """
+                    zip -q -r ${component}-${appVersion}.zip * -x Jenkinsfile -x ${component}-${appVersion}.zip
+                    ls -ltr
+                    """
                 }
             }
-            //sonar-scanner command expect sonar-project.properties should be available
-            stage('Sonar Scan') {
-                steps {
-                    echo "Sonar scan done"
+            stage('Docker build'){
+                steps{
+
+                    sh """
+                        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
+                        docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${project}-${component}:${appVersion} .
+                        docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/${project}-${component}:${appVersion}
+                    """
                 }
             }
-            stage('Build') {
-                steps {
-                    sh 'ls -ltr'
-                    sh "zip -r ${component}.zip ./* --exclude=.git --exclude=.zip"
-                }
-            }
-            stage('SAST') {
-                steps {
-                    echo "SAST Done"
-                    echo "package version: $packageVersion"
-                }
-            }
-            stage('Docker Build') {
-                steps {
+
+            stage('Deploy'){
+                steps{
                     script{
-                        sh """
-                            docker build -t joindevops/${component}:${packageVersion} .
-                        """
+                        releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
+                        if(releaseExists.isEmpty()){
+                            echo "${component} not installed yet, first time installation"
+                            sh"""
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm install ${component} -n ${project} .
+                            """
+                        }
+                        else{
+                            echo "${component} exists, running upgrade"
+                            sh"""
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm upgrade ${component} -n ${project} .
+                            """
+                        }
                     }
                 }
             }
-            stage('Docker Push') {
-                steps {
+            stage('Verify Deployment'){
+                steps{
                     script{
-                        sh """
-                            docker push joindevops/${component}:${packageVersion}
-                        """
+                        rollbackStatus = sh(script: "kubectl rollout status deployment/backend -n ${project} --timeout=1m || true", returnStdout: true).trim()
+                        if(rollbackStatus.contains('successfully rolled out')){
+                            echo "Deployment is successfull"
+                        }
+                        else{
+                            echo "Deployment is failed, performing rollback"
+                            if(releaseExists.isEmpty()){
+                                error "Deployment failed, not able to rollback, since it is first time deployment"
+                            }
+                            else{
+                                sh """
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                helm rollback backend -n ${project} 0
+                                sleep 60
+                                """
+                                rollbackStatus = sh(script: "kubectl rollout status deployment/backend -n expense --timeout=2m || true", returnStdout: true).trim()
+                                if(rollbackStatus.contains('successfully rolled out')){
+                                    error "Deployment is failed, Rollback is successfull"
+                                }
+                                else{
+                                    error "Deployment is failed, Rollback is failed"
+                                }
+                            }
+                        }
                     }
                 }
             }
-            stage('EKS Login') {
-                steps {
+            
+            /* stage('Nexus Artifact Upload'){
+                steps{
                     script{
-                        sh """
-                            aws eks update-kubeconfig --region us-east-1 --name roboshop-dev
-                        """
+                        nexusArtifactUploader(
+                            nexusVersion: 'nexus3',
+                            protocol: 'http',
+                            nexusUrl: "${nexusUrl}",
+                            groupId: 'com.expense',
+                            version: "${appVersion}",
+                            repository: "backend",
+                            credentialsId: 'nexus-auth',
+                            artifacts: [
+                                [artifactId: "backend" ,
+                                classifier: '',
+                                file: "backend-" + "${appVersion}" + '.zip',
+                                type: 'zip']
+                            ]
+                        )
                     }
-                }
-            }
-            stage('EKS Deploy') {
-                steps {
-                    script{
-                        sh """
-                            cd helm
-                            sed -i 's/IMAGE_VERSION/$packageVersion/g' values.yaml
-                            helm upgrade --install ${component} -n roboshop .
-                        """
-                    }
-                }
-            }
-            //install pipeline utility steps plugin, if not installed
-            /* stage('Publish Artifact') {
-                steps {
-                    nexusArtifactUploader(
-                        nexusVersion: 'nexus3',
-                        protocol: 'http',
-                        nexusUrl: pipelineGlobals.nexusURL(),
-                        groupId: 'com.roboshop',
-                        version: "$packageVersion",
-                        repository: "${component}",
-                        credentialsId: 'nexus-auth',
-                        artifacts: [
-                            [artifactId: "${component}",
-                            classifier: '',
-                            file: "${component}.zip",
-                            type: 'zip']
-                        ]
-                    )
                 }
             } */
-
-            
         }
-        post{
-            always{
-                echo 'cleaning up workspace'
-                //deleteDir()
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            success { 
+                echo 'I will run when pipeline is success'
+            }
+            failure { 
+                echo 'I will run when pipeline is failure'
             }
         }
     }
