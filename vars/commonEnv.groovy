0f8c8024faa6e1e457eaca9900970a6ce6553cbb
hi-to-cloud <hi.to.cloud@gmail.com>
2025-01-22 07:49:36 +0530

    Create commonEnv.groovy

---
+++ vars/commonEnv.groovy
@@ -0,0 +1,20 @@
+def call(){
+  env.NK = UUID.randomUUID().toString()
+  env.TKN = credentials('tkn-007')
+  env.PIPELINE_RUN_ID = UUID.randomUUID().toString()
+  env.SETUP_CONTAINER = "scripts/setupContainer.sh"
+  env.RUN_TESTS = "scripts/runTests.sh"
+  env.CHECK_TESTS = "scripts/checkTests.sh"
+  env.GET_PIPELINE_INFO = "scripts/getPipelineInfo.py"
+  env.TESTS = "/usr/bin/tests"
+  env.DASHBOARD_LINK = "Failed to create dashboard link"
+  env.SUMMARY = "SITF Jenkins Pipeline Summary"
+  env.FASTTRACK = "false"
+  env.RUN_COUNT = 0
+  env.JIRA_LIST = "No test failures"
+  env.SLACK_COLOUR = "good"
+  env.QUALITY_LINK = "Build Quality Dashboard"
+  env.BRANCH = ""
+  env.RELEASE = ""
+  env.PRE_MERGE_TEST = "false"
+}
