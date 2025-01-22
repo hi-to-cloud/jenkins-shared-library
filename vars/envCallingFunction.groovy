e67933a681b53a66fe802bf9219d8df78a0787c8
hi-to-cloud <hi.to.cloud@gmail.com>
2025-01-22 13:14:13 +0530

    Update envCallingFunction.groovy

--- vars/envCallingFunction.groovy
+++ vars/envCallingFunction.groovy
@@ -1,9 +1,10 @@
-def call(String SYSTEM_NAME){
+def call(String SYSTEM_NAME, String DO_PUBLISH = 'false'){
     pipeline {
         agent any
         parameters {
             string(name:"SYSTEM_NAME", defaultValue: SYSTEM_NAME, description:'System to run on. Current option is '+ SYSTEM_NAME )
             string(name:"SYSTEM_NAME2", defaultValue:'SYSTEM_NAME2', description:'System to run on. Current option is $SYSTEM_NAME.')
+            string(name: "DO_PUBLISH", defaultValue: 'false', description: "System to run on. Current option is ${SYSTEM_NAME}.")
         }
         environment {
             SYSTEM = sh(script: "echo '${SYSTEM_NAME}' | awk -F '-' '{print \$NF}'", returnStdout: true).trim()
